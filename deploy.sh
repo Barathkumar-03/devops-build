a66de1e3869bdb04fe1207387b26ad356bb97fc0
Barath Kumar Kumar <168456667+Barathkumar-03@users.noreply.github.com>
2025-03-31 02:11:23 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-echo "Deploying to server..."
-docker-compose down
-docker-compose up -d
+
+echo "Deploying dev environment..."
+docker run -d -p 80:80 barathkumar1207/dev:latest
+
+echo "Deploying prod environment..."
+docker run -d -p 8080:80 barathkumar1207/prod:latest
